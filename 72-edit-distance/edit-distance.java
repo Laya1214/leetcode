fd4e32c9776c6bf9cbd2b5c169305dba8adce23c
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-03-27 16:04:45 +0530

    Time: 5 ms (68.82%) | Memory: 47.2 MB (45.24%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=1;i<=n;i++)dp[i][0]=i;
+        for(int j=1;j<=m;j++)dp[0][j]=j;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    int cor=dp[i-1][j-1];
+                    int top=dp[i-1][j];
+                    int left=dp[i][j-1];
+                    dp[i][j]=Math.min(cor,Math.min(top,left))+1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
