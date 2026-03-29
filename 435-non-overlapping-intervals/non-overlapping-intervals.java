c202c88bafc994e008896fb26a34b23f31046daa
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-03-29 20:48:48 +0530

    Time: 53 ms (16.51%) | Memory: 115.8 MB (68.19%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(intervals,Comparator.comparingInt(i->i[1]));
         int count=1;
         int prev=0;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             if(intervals[i][0] >= intervals[prev][1]){
                 prev=i;
                 count++;
