c6ec564c3808545e5d250374965ca2fdb4374aa7
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-06-04 16:45:48 +0530

    Time: 376 ms (28.58%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,5 @@
-SELECT m.name
-FROM Employee e
-left JOIN Employee m ON e.managerId = m.id
-GROUP BY m.id
-HAVING COUNT(m.id) >= 5;
+Select e1.name from Employee e2
+LEFT JOIN Employee e1 ON
+e2.managerId = e1.id
+GROUP BY e1.id
+HAVING count(e1.id) >= 5;
