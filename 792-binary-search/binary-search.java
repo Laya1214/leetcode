d3ddfadb9371a003ac3709d9d4fd2c606f34b78f
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-06-04 13:01:19 +0530

    Time: 3 ms (1.49%) | Memory: 48.4 MB (48.84%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,8 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==1 && nums[0]==target)return 0;
         int left=0;
         int right=nums.length-1;
+        if(right==0 && nums[0]==target)return 0;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target)return mid;
