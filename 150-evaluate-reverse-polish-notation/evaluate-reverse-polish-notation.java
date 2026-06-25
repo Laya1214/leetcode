761ea0182ff4300c6b7ccc67e4834ec2de946459
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-06-25 14:00:05 +0530

    Time: 6 ms (88.44%) | Memory: 45.4 MB (29.07%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int evalRPN(String[] s) {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<s.length;i++){
+            if(s[i].equals("+")){
+                st.push(st.pop()+st.pop());
+            }
+            else if(s[i].equals("-")){
+                int b = st.pop(); 
+                int a = st.pop(); 
+                st.push(a - b);
+            }
+            else if(s[i].equals("*")){
+                st.push(st.pop()*st.pop());
+            }
+            else if(s[i].equals("/")){
+                int b = st.pop(); 
+                int a = st.pop(); 
+                st.push(a / b);
+            }
+            else{
+                st.push(Integer.parseInt(s[i]));
+            }
+        }
+        return st.peek();
+    }
+}
