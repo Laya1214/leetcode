b101f913f739e063ea54b1f8efdbad540491ae4c
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-06-30 15:47:17 +0530

    Time: 7 ms (21.08%) | Memory: 47.1 MB (5.22%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(n -> n.val));
+        for(ListNode li:lists){
+            if(li!=null){
+                pq.offer(li);
+            }
+        }
+        ListNode dummy=new ListNode(-1);
+        ListNode x=dummy;
+        while(!pq.isEmpty()){
+            ListNode cur=pq.poll();
+            x.next=cur;
+            x=x.next;
+            if (cur.next != null) {
+                pq.offer(cur.next);
+            }
+        }
+    return dummy.next;
+    }
+}
