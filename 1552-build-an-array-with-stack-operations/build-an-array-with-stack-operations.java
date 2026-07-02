a8d8945d485904595cdab142c765bfef788afad8
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-07-02 15:45:06 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (84.99%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         List<String> s=new ArrayList<>();
-        Stack<Integer> st=new Stack<>();
+        
         int tl=target.length;
         int tindex=0;
         for(int i=1;i<=n;i++){
@@ -9,13 +9,12 @@ class Solution {
                 break;
             }else{
                 if(target[tindex]==i){
-                    st.push(i);
+                   
                     s.add("Push");
                     tindex++;
                 }
                 else{
-                    st.push(i);
-                    st.pop();
+                    
                     s.add("Push");
                     s.add("Pop");
                 }
