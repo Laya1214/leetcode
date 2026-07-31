4173f7ff62403f5b7cfb924828c9d5a46f5b10e3
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-07-31 10:14:23 +0530

    Time: 1 ms (2.67%) | Memory: 46.7 MB (66.22%) - LeetSync

--- 669-trim-a-binary-search-tree/trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.java
@@ -15,16 +15,28 @@
  */
 class Solution {
     public TreeNode trimBST(TreeNode root, int low, int high) {
-       if(root==null)return null;
-       if(root.val<low){
-            return trimBST(root.right,low,high);
-       } 
-       if(root.val>high){
-        return trimBST(root.left,low,high);
-       }
-       root.left=trimBST(root.left,low,high);
-       root.right=trimBST(root.right,low,high);
-       return root;
+        if(root==null)return null;
+      List<Integer> list=new ArrayList<>();
+      pre(root,list,low,high);
+      if(list.isEmpty())return null;
+      TreeNode root1=new TreeNode(list.get(0));
+      for(int i=1;i<list.size();i++){
+        insert(root1,list.get(i));
+      }
+      return root1;
     }
-    
+    public void pre(TreeNode root,List<Integer> list,int low,int high){
+        if(root==null)return;
+        if(root.val>=low  && root.val<=high){
+            list.add(root.val);
+        }
+        pre(root.left,list,low,high);
+        pre(root.right,list,low,high);
+    }
+    public TreeNode insert(TreeNode root,int val){
+        if(root==null){ return new TreeNode(val);}
+        if(root.val<val)root.right=insert(root.right,val);
+        else root.left=insert(root.left,val);
+        return root;
+    }
 }
