1cb658a0272731ebc0f0836fad8c0844e24fd15e
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-08-03 20:22:29 +0530

    Time: 3 ms (14.89%) | Memory: 46.4 MB (26.31%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -3,14 +3,15 @@ class Solution {
         int n=rooms.size();
         boolean[] visited=new boolean[n];
         Queue<Integer> q=new LinkedList<>();
+        Set<Integer> v=new HashSet<>();
         q.offer(0);
-        visited[0]=true;
+        v.add(0);
         int vc=1;
         while(!q.isEmpty()){
             int cur=q.poll();
             for(int k:rooms.get(cur)){
-                if(!visited[k]){
-                    visited[k]=true;
+                if(!v.contains(k)){
+                    v.add(k);
                     q.offer(k);
                     vc++;
                 }
