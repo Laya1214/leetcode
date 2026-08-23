529db1267d316658b79a3a1e69ea4671a8277cab
laya Jujjuvarapu <layajujjuvarapu@gmail.com>
2026-08-23 11:00:44 +0530

    Time: 10 ms (16.06%) | Memory: 47.1 MB (41.61%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int s1=0;
+        int s2=0;
+        int c1=0;
+        int c2=0;
+        int left=0;
+        int right=num.length()-1;
+        while(left<right){
+            if(num.charAt(left)=='?'){
+                c1++;
+            }
+            else{
+                int n=num.charAt(left)-'0';
+                s1+=n;
+            }
+            if(num.charAt(right)=='?'){
+                c2++;
+            }
+            else{
+                int n1=num.charAt(right)-'0';
+                s2+=n1;
+            }
+            left++;
+            right--;
+        }
+        int sum=s1-s2;
+        int count=9*((c2-c1)/2);
+        if((c1+c2)%2!=0)return true;
+        
+        else{
+            if(sum==count)return false;     
+        }
+        return true;
+    }
+}
